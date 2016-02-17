85e3b68c62cf97fe77119262cdd86a50d755a609
Dennis Teichert <dteichert@zalando-20190.corp.ad.zalando.net>
2016-02-17 17:29:14 +0100

    4th commit

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -14,6 +16,17 @@ public class MyFirstTest {
         WebDriver driver = new FirefoxDriver();
 
         driver.navigate().to("http://de.release.zlounge.org");
+        Assert.assertTrue("title should start with Exklusive Mode",
+                driver.getTitle().startsWith("Exklusive Mode"));
+
+        WebElement email=driver.findElement(By.id("f2-ie-mail"));
+        email.sendKeys("dt_selenium@qa-zalando.de");
+
+        WebElement password=driver.findElement(By.id("f2-ip-password"));
+        password.sendKeys("123456");
+
+        driver.findElement(By.id("submit-login")).click();
+
         Assert.assertTrue("title should start with Exklusive Mode",
                 driver.getTitle().startsWith("Exklusive Mode"));
 
